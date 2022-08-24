937481d83ab3fbc78d1a452cc6647bbf763bedfd
marc-hg <90116640+marc-hg@users.noreply.github.com>
2022-08-24 22:44:26 +0200

    fix zsh

--- src/install_zsh.sh
+++ src/install_zsh.sh
@@ -14,6 +14,7 @@ chsh -s $(which zsh)
 # Install oh my zsh
 if [ -d "~/.oh-my-zsh" ]
 then
+  echo The $ZSH folder already exists, cloning into ~/.your-old-oh-my-zsh-${DATE}...
   mkdir ~/.your-old-oh-my-zsh-${DATE}
   mv ~/.oh-my-zsh ~/.your-old-oh-my-zsh-${DATE}
 fi
