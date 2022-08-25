48b66bfe3d33948ff8c7986c14a1ee73a70cf7b5
marc-hg <marcoshornogarcia@gmail.com>
2022-08-25 21:42:07 +0200

    improve makefile

--- src/install_nvim.sh
+++ src/install_nvim.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
-
-wget https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.deb
+echo 'Installing deps...'
 echo 'Enter your sudo password...'
+sudo apt install wget -y
+wget https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.deb
 sudo apt install ./nvim-linux64.deb
 export PATH=$PATH:~/.local/bin
 bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
