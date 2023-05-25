8b01afd3483f9baebb3f8ee01821d32ba2dec6b2
myuser <notexist@wo.wa>
2023-05-25 23:02:52 +0200

    asdf

--- i3/.config/i3/open-work-media.sh
+++ i3/.config/i3/open-work-media.sh
@@ -3,8 +3,8 @@ i3-msg 'workspace 4; exec google-chrome --user-data-dir="~/.config/google-chrome
 sleep 2
 i3-msg 'split v'
 sleep 2
-i3-msg 'exec google-chrome --user-data-dir="~/.config/google-chrome/KioskProfile" --app="https://calendar.google.com/"'
+i3-msg 'exec google-chrome --user-data-dir="~/.config/google-chrome/KioskProfile" --app="https://mail.google.com/"'
 sleep 2
 i3-msg 'split h'
 sleep 2
-i3-msg 'exec google-chrome --user-data-dir="~/.config/google-chrome/KioskProfile" --app="https://mail.google.com/"'
+i3-msg 'exec google-chrome --user-data-dir="~/.config/google-chrome/KioskProfile" --app="https://calendar.google.com/"'
