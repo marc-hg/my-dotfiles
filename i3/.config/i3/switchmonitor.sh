37f6315f3ab3c4084a69b45046a904d8cf11f930
myuser <myuser.notexist.wo.wa>
2023-06-06 16:54:18 +0200

    adfs

--- i3/.config/i3/switchmonitor.sh
+++ i3/.config/i3/switchmonitor.sh
@@ -6,10 +6,12 @@ output=$(xrandr --query | grep "HDMI-1 connected" | cut -d ' ' -f2)
 if [ "$output" = "connected" ]; then
   # If the external monitor is disconnected, switch to external monitor only
   xrandr --output eDP-1 --off --output HDMI-1 --mode 2560x1080 --rate 74.99 --auto --primary
-  pactl set-card-profile alsa_card.pci-0000_00_1f.3 output:hdmi-stereo
+  # pactl set-card-profile alsa_card.pci-0000_00_1f.3 output:hdmi-stereo
+  feh -feh --randomize --bg-fill ~/my-dotfiles/backgrounds/default.jpg
 else
   # If the external monitor is connected, switch to laptop screen only
   xrandr --output HDMI-1 --off --output eDP-1 --auto --primary
-  pactl set-card-profile alsa_card.pci-0000_00_1f.3 output:analog-stereo+input:analog-stereo
+  feh -feh --randomize --bg-fill ~/my-dotfiles/backgrounds/default.jpg
+  # pactl set-card-profile alsa_card.pci-0000_00_1f.3 output:analog-stereo+input:analog-stereo
 fi
 feh --randomize --preload --bg-fill ~/Pictures/Wallpapers/
