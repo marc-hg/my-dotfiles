a56fa53c2a3461df315faa12911cdf92a5139b18
myuser <myuser.notexist.wo.wa>
2023-08-09 10:28:24 +0200

    asdf

--- i3/.config/i3/open-work-media.sh
+++ i3/.config/i3/open-work-media.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-i3-msg 'workspace 4; exec google-chrome-beta --app="https://app.slack.com/client/"'
+i3-msg 'workspace 4; exec google-chrome --app="https://app.slack.com/client/"'
 sleep 2
 i3-msg 'split v'
 sleep 2
-i3-msg 'exec google-chrome-beta --app="https://calendar.google.com/"'
+i3-msg 'exec google-chrome --app="https://calendar.google.com/"'
 sleep 2
 i3-msg 'split h'
 sleep 2
-i3-msg 'exec google-chrome-beta --app="https://mail.google.com/"'
+i3-msg 'exec google-chrome --app="https://mail.google.com/"'
--- i3/.config/i3/switchmonitor.sh
+++ i3/.config/i3/switchmonitor.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Get the current state of the external monitor
-output=$(xrandr --query | grep "HDMI-1 connected" | cut -d ' ' -f2)
+output=$(xrandr --query | grep "DP-1 connected (" | cut -d ' ' -f2)
 
 if [ "$output" = "connected" ]; then
   # If the external monitor is disconnected, switch to external monitor only
-  xrandr --output eDP-1 --off --output HDMI-1 --mode 2560x1080 --rate 74.99 --auto --primary
+  xrandr --output eDP-1 --off --output DP-1 --mode 2560x1080 --rate 74.99 --auto --primary
   # pactl set-card-profile alsa_card.pci-0000_00_1f.3 output:hdmi-stereo
   feh -feh --randomize --bg-fill ~/my-dotfiles/backgrounds/default.jpg
 else
   # If the external monitor is connected, switch to laptop screen only
-  xrandr --output HDMI-1 --off --output eDP-1 --auto --primary
+  xrandr --output DP-1 --off --output eDP-1 --auto --primary
   feh -feh --randomize --bg-fill ~/my-dotfiles/backgrounds/default.jpg
   # pactl set-card-profile alsa_card.pci-0000_00_1f.3 output:analog-stereo+input:analog-stereo
 fi
